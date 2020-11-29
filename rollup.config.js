3e379f718f6e407242f6bd59220323bc6c8981fb
Alejandro <alexsotog82@gmail.com>
2020-11-29 11:28:37 -0600

    modify gitignore and add new plugin for minify js

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
 import copy from 'rollup-plugin-copy';
+import uglify from '@lopatnov/rollup-plugin-uglify';
 
 const name = `${((new Date()).getTime())}.bundled.js`;
 
 const filesizeConfig = {
     showGzippedSize: true,
-    showBrotliSize: false,
+    showBrotliSize: true,
     showMinifiedSize: true,
 };
 
@@ -26,7 +27,22 @@ const config = {
     output: {
         file: `dist/${name}`
     },
-    plugins: [ resolve(), copy(copyConfig), filesize(filesizeConfig) ]
+    plugins: [ 
+            resolve(),
+            copy(copyConfig), 
+            uglify({
+                output: {
+                    comments: function(node, comment) {
+                        if (comment.type === "comment2") {
+                            // multiline comment
+                            return /@preserve|@cc_on/i.test(comment.value);
+                        }
+                        return false;
+                    }
+                }
+            }),
+            filesize(filesizeConfig)
+        ]
 };
 
 export default config;
