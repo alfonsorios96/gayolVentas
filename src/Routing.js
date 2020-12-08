16861cdd12153f980e002c542b8c862eba790679
Alejandro <alexsotog82@gmail.com>
2020-12-08 13:52:23 -0600

    feat: children in routing and refactor

--- src/Routing.js
+++ src/Routing.js
@@ -3,37 +3,39 @@ import { Router } from '@vaadin/router';
 
 const routes = [
     {
-        path: '/',
+        path: '',
         component: 'gayol-app',
-        action: async() => {
-            await import('./GayolApp');
-        }
-    },
-    // TODO: CAMBIAR DASBOARDPAGE POR HOME PAGE
-    {
-        path: 'dashboard',
-        component: 'dashboard-page',
-        action: async() => await import ('./Pages/DashboardPage')
-    },
-    {
-        path: 'home',
-        component: 'home-page',
-        action: async() => await import ('./Pages/HomePage')
-    },
-    {
-        path: 'searchpage',
-        component: 'search-page',
-        action: async() => await import ('./Pages/SearchPage')
-    },
-    {
-        path: 'list',
-        component: 'list-page',
-        action: async() => await import ('./Pages/ListPage')
-    },
-    {
-        path: 'upload',
-        component: 'upload-list',
-        action: async() => await import ('./Pages/UploadList')
+        children: [
+            {
+                path: '',
+                redirect: '/dashboard',
+            },
+            {
+                path: 'dashboard',
+                component: 'dashboard-page',
+                action: async() => await import('./Pages/DashboardPage')
+            },
+            {
+                path: 'home',
+                component: 'home-page',
+                action: async() => await import('./Pages/HomePage')
+            },
+            {
+                path: 'list',
+                component: 'list-page',
+                action: async() => await import('./Pages/ListPage')
+            },
+            {
+                path: 'uploadList',
+                component: 'upload-List',
+                action: async() => await import('./Pages/UploadList')
+            },
+            {
+                path: 'search',
+                component: 'search-page',
+                action: async() => await import('./Pages/SearchPage')
+            }
+        ]
     },
     {
         path: '(.*)',
